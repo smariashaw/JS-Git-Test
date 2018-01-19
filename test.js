ca876ed68b45097b260c97075d025f17f16bf599
smariashaw <smariashaw@gmail.com>
2018-01-19 16:35:05 -0500

    test js

---
+++ test.js
@@ -0,0 +1,14 @@
+
+// this is a single line comment//
+console.log (' something funn');
+
+/* multiple lines
+of code like this */
+var sum = 128 + 17 + 50;
+var average = sum / 3;
+
+/* this is just a practise
+example*/
+var sum = 789 /12345;
+console.log (sum);
+
